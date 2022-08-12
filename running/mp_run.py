b589bd66ac580cb73dd97ae0191e9c8d09beef7c
mati <matiasbilkis@gmail.com>
2022-08-12 00:46:52 -0300

    mp working

--- running/mp_run.py
+++ running/mp_run.py
@@ -9,7 +9,11 @@ cores = 4
 js = np.linspace(-1.4,2.,16)
 
 def send_vans(J):
-    os.system("python3 running/main.py --params {} --n_qubits 8".format([1.0, J]))
+    #print(J)
+    #print("python3 running/main.py --params '{}' --n_qubits 8".format(str([1.0, J])))
+    os.system("python3 running/main.py --params '{}' --n_qubits 8".format(str([1.0, J])))
+
+    # os.system("python3 running/main.py --params {} --n_qubits 8".format(str([1.0, J])))
 
 with mp.Pool(cores) as p:
     p.map(send_vans, js)
