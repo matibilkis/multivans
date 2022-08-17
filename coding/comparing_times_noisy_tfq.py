a617e080ff3360ad6baaa7a950a246f82c8f6fa4
mati <matiasbilkis@gmail.com>
2022-08-17 13:56:46 -0300

    new layer ExpectationNoise, works slightly faster than NoisyPQC. Will add this to VQE

--- coding/comparing_times_noisy_tfq.py
+++ coding/comparing_times_noisy_tfq.py
@@ -119,13 +119,25 @@ def get_grad():
     tape.gradient(p,values)
 %timeit get_grad()
 
+translator = tfq_translator.TFQTranslator(n_qubits = 8, initialize="x")#, device_name="forest.numpy_wavefunction")
+minimizer = tfq_minimizer.Minimizer(translator, mode="VQE", hamiltonian = problem, params = params, lr=learning_rate, shots=shots, g=g, J=J, patience=100, max_time_training=600)
+circuit_db = translator.initialize(mode="x")
+circuit, circuit_db = translator.give_circuit(translator.db_train, unresolved=True)
+nois = circuit + cirq.Circuit(cirq.depolarize(.01).on_each(*circuit.all_qubits()))
+values = np.array([database.get_trainable_params_value(translator,circuit_db)])
+noisy = tfq.convert_to_tensor([nois])
+symbols = database.get_trainable_symbols(translator, circuit_db)
+
 
 class NoisyAvg(tf.keras.layers.Layer):
-    def __init__(self, units, observable, symbols):
+    def __init__(self, units, observable, symbols, init_params=None):
         super(NoisyAvg, self).__init__()
         self.w = self.add_weight(
             shape=(1, units), initializer="random_normal", trainable=True
         )
+        if not (init_params is None):
+           self.tvar[0].assign(tf.Variable(tf.convert_to_tensor(initial_params.astype(np.float32))))
+
         self.samples = np.array([[1000]*len(observable)])
         self.symbols = tf.convert_to_tensor(symbols)
         self.observable = tfq.convert_to_tensor([observable])
@@ -136,81 +148,40 @@ class NoisyAvg(tf.keras.layers.Layer):
 
 lay = NoisyAvg(8, minimizer.observable, symbols)
 
-lay(noisy)
-
 ### what happens if i create a model ? (maybe compiling gets thing faster ?)
 
 class model(tf.keras.Model):
     def __init__(self, observable, symbols, initial_params=None ):
         super(model,self).__init__()
-        self.lay = NoisyAvg(8, minimizer.observable, symbols)
-#        tfq.differentiators.ForwardDifference().generate_differentiable_op(sampled_op = tfq.noise.expectation)
-        self.observable = tfq.convert_to_tensor([observable])
-        self.samples = np.array([[1000]*len(observable)])
-
-        self.symbols = tf.convert_to_tensor(symbols)
-        #self.tvar = [tf.ones(len(symbols))]
-        self.tvar =tf.Variable(initial_value=tf.ones((1,len(symbols))))#random.uniform((1,len(symbols))), trainable=True)
-
-        #if not (initial_params is None):
-    #        self.tvar[0].assign(tf.Variable(tf.convert_to_tensor(initial_params.astype(np.float32))))
+        self.lay = NoisyAvg(len(symbols), minimizer.observable, symbols)
 
     def call(self, inputs):
         """
         inputs: tfq circuits (resolved or not, to train one has to feed unresolved).
         """
-        f = tf.math.reduce_sum(self.lay( inputs),axis=-1)#,  self.symbols, self.tvar, self.observable, self.samples), axis=-1)
+        f = self.lay(inputs)#,  self.symbols, self.tvar, self.observable, self.samples), axis=-1)
         return f
 
     def train_step(self, data):
         x,y=data
         with tf.GradientTape() as tape:
-            tape.watch(self.tvar)
+            tape.watch(self.trainable_variables)
             preds = self(x)#,training=True)
             cost = self.compiled_loss(preds, preds) #notice that compiled loss takes care only about the preds
-        grads=tape.gradient(cost,self.tvar)
-        self.optimizer.apply_gradients(zip(grads, self.tvar))
+        grads=tape.gradient(cost,self.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
         return {k.name:k.result() for k in self.metrics}
 
-translator = tfq_translator.TFQTranslator(n_qubits = 8, initialize="x")#, device_name="forest.numpy_wavefunction")
-minimizer = tfq_minimizer.Minimizer(translator, mode="VQE", hamiltonian = problem, params = params, lr=learning_rate, shots=shots, g=g, J=J, patience=100, max_time_training=600)
-circuit_db = translator.initialize(mode="x")
-circuit, circuit_db = translator.give_circuit(translator.db_train, unresolved=True)
-nois = circuit + cirq.Circuit(cirq.depolarize(.01).on_each(*circuit.all_qubits()))
-values = np.array([database.get_trainable_params_value(translator,circuit_db)])
-noisy = tfq.convert_to_tensor([nois])
-
-symbols = database.get_trainable_symbols(translator, circuit_db)
 momo = model(minimizer.observable,symbols)
-
 momo.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),loss= EnergyLoss())
 momo(noisy)
 
 
-momo.trainable_variables
-
-
-momo.optimizer
-
-
-#momo.train_step([noisy,noisy])
-
-
-#%timeit momo.train_step([noisy, noisy])
-
-with tf.GradientTape() as t:
-    t.watch(momo.tvar)
-    g = momo(noisy)
-    c = momo.compiled_loss(g,g)
-gg = t.gradient(c, momo.tvar)
-
-gg
-momo.optimizer.apply_gradients(zip(gg, momo.tvar))
+momo.train_step([noisy,noisy])
+%timeit momo.train_step([noisy, noisy])
 
 
 momo.fit(x=noisy, y=noisy, epochs=10)
-
-
 
 
 
