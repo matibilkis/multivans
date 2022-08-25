20ebc3f59148904c6a05d63d5e6fbee703f6bd8f
matibilkis <matiasbilkis@gmail.com>
2022-08-25 03:00:49 +0200

    sending jobs OK to PIC

--- running/HPC/HEA/subash_HEA.sh
+++ running/HPC/HEA/subash_HEA.sh
@@ -3,7 +3,7 @@ hea=$1
 cd ~/multivans
 . ~/vans_env/bin/activate
 START=$(date +%s.%N)
-python3 running/tfq/mp_HEA.py --L_HEA $hea
+python3 running/tfq/HEA/mp_HEA.py --L_HEA $hea
 END=$(date +%s.%N)
 DIFF=$(echo "$END - $START" | bc)
 echo $DIFF
--- running/tfq/.ipynb_checkpoints/mp_run-checkpoint.py
+++ running/tfq/.ipynb_checkpoints/mp_run-checkpoint.py
@@ -3,24 +3,21 @@ import sys
 sys.path.insert(0, os.getcwd())
 import multiprocessing as mp
 import numpy as np
-import argparse
-
 # cores = mp.cpu_count()
-### NOTE i don't use HEA input any more, since i minimize sequentially
 
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument("--L_HEA", type=int, default=1)
-args = parser.parse_args()
-L_HEA = args.L_HEA
+cores = 4
+js = np.linspace(0.,2.,16)
+# js = [.4]
+# js = np.linspace(0,.2,4)#list(range(1,8,1))
 
-cores = 8
-js = np.array([0] + list(np.logspace(-7,-2,7)))
+# js = [0.0, 0.001, 0.01, 0.1]
 
 def send_vans(ns):
+    print(ns)
     #print("python3 running/main.py --params '{}' --n_qubits 8".format(str([1.0, J])))
     # os.system("python3.8 running/tfq/tfq_main.py --params '{}' --n_qubits 8 --problem TFIM --itraj {}".format(str([1.0, itraj]), 1))
-    os.system("python3 running/tfq/HEA/main_HEA.py --params '{}' --n_qubits 4 --problem TFIM --itraj 0 --noisy 1 --noise_strength {} --vans_its 40 --L_HEA {}".format(str([1.0, 1.0]), ns, L_HEA))
-#    os.system("python3 running/tfq/main_HEA.py --params '{}' --n_qubits 4 --problem TFIM --itraj 0 --noisy 1 --noise_strength {} --vans_its 40 --L_HEA {}".format(str([1.0, 1.0]), ns, L_HEA))
+    # os.system("python3.8 running/tfq/tfq_main.py --params '{}' --n_qubits 4 --problem TFIM --itraj 0 --noisy 1 --noise_strength {} --vans_its 40".format(str([1.0, 1.0]), ns))
+    os.system("python3.8 running/tfq/tfq_main.py --params '{}' --n_qubits 4 --problem TFIM --itraj 0 --noisy 0 --vans_its 100".format(str([1.0, ns])))
 
     # os.system("python3 running/main.py --params {} --n_qubits 8".format(str([1.0, J])))
 
