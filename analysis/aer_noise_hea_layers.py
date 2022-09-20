90ed7d3836ba258b91ce94401092a67a8431f9d4
matibilkis <matiasbilkis@gmail.com>
2022-09-20 05:11:29 +0200

    HEA 8 qubits sending jobs

--- analysis/aer_noise_hea_layers.py
+++ analysis/aer_noise_hea_layers.py
@@ -104,12 +104,87 @@ def load_ev_vans(ns, itraj=1):
     evaluator = tfq_evaluator.PennyLaneEvaluator(minimizer = minimizer, killer=killer, inserter = inserter, args=args_evaluator, lower_bound=translator.ground, stopping_criteria=1e-3, vans_its=args.vans_its, acceptange_percentage = acceptange_percentage)
 
     evaluator.load_dicts_and_displaying(evaluator.identifier)
+    return evaluatorimport os
+import sys
+import numpy as np
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow as tf
+import tensorflow_quantum as tfq
+import cirq
+from datetime import datetime
+sys.path.insert(0, os.getcwd())
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import pennylane as qml
+
+from tqdm import tqdm
+import utilities.translator.tfq_translator as tfq_translator
+import utilities.evaluator.evaluator as tfq_evaluator
+import utilities.variational.tfq.variational as tfq_minimizer
+import utilities.simplification.simplifier as penny_simplifier
+import utilities.simplification.misc as simplification_misc#.kill_and_simplify
+import utilities.simplification.tfq.gate_killer as tfq_killer
+import utilities.database.database as database
+import utilities.database.templates as templates
+import utilities.mutator.idinserter as idinserter
+import running.misc.misc as miscrun
+import argparse
+import ast
+from importlib import reload
+
+
+from utilities.evaluator.misc import *
+import pickle
+
+
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+
+
+
+def load_ev_HEA(ns, itraj=1,NQ=8, name=""):
+
+    args = {"problem":"TFIM", "params":"[1.,1.]","nrun":0, "shots":0, "epochs":500, "n_qubits":NQ, "vans_its":200,"itraj":itraj, "noisy":True,
+            "noise_strength":ns, "acceptange_percentage": 0.01, "L_HEA":1,"run_name":name,"noise_model":"aer"}
+    args = miscrun.FakeArgs(args)
+    L_HEA = args.L_HEA
+    problem = args.problem
+    params = ast.literal_eval(args.params)
+    shots = miscrun.convert_shorts(args.shots)
+    epochs = args.epochs
+    n_qubits = args.n_qubits
+    learning_rate=1e-4
+    acceptange_percentage = args.acceptange_percentage
+    noise_strength = args.noise_strength
+    int_2_bool = lambda x: True if x==1 else False
+    noisy = int_2_bool(args.noisy)
+    tf.random.set_seed(abs(args.itraj))
+    np.random.seed(abs(args.itraj))
+
+    translator = tfq_translator.TFQTranslator(n_qubits = n_qubits, initialize="x", noisy=args.noisy, noise_strength = noise_strength, noise_model="aer")#, device_name="forest.numpy_wavefunction")
+    translator_killer = tfq_translator.TFQTranslator(n_qubits = translator.n_qubits, initialize="x", noisy=translator.noisy, noise_strength = args.noise_strength, noise_model="aer")
+    minimizer = tfq_minimizer.Minimizer(translator, mode="VQE", hamiltonian = problem, params = params, lr=learning_rate, shots=shots, patience=30, max_time_training=600, verbose=0)
+    simplifier = penny_simplifier.PennyLane_Simplifier(translator)
+    killer = tfq_killer.GateKiller(translator, translator_killer, hamiltonian=problem, params=params, lr=learning_rate, shots=shots, accept_wall = 2/args.acceptange_percentage)
+    inserter = idinserter.IdInserter(translator, noise_in_rotations=1e-1, mutation_rate = 1.5)
+    args_evaluator = {"n_qubits":translator.n_qubits, "problem":problem,"params":params,"nrun":args.itraj,"name":"HEA_fixed"}
+    evaluator = tfq_evaluator.PennyLaneEvaluator(minimizer = minimizer, killer=killer, inserter = inserter, args=args_evaluator, lower_bound=translator.ground, stopping_criteria=1e-3, vans_its=args.vans_its, acceptange_percentage = acceptange_percentage)
+
+    evaluator.load_dicts_and_displaying(evaluator.identifier)
     return evaluator
 
-js = list(np.logspace(-3.7,-2.9,16))
+
+js = list(np.logspace(-8,-5,16))
 
 
-evo = load_ev_HEA(js[0], itraj=1)
+evo = load_ev_HEA(js[0], itraj=51)
 
 
 evo = load_ev_HEA(j, itraj=1)
--- running/tfq/HEA/mp_HEA_fixed.py
+++ running/tfq/HEA/mp_HEA_fixed.py
@@ -13,12 +13,12 @@ parser.add_argument("--itraj", type=int, default=1)
 args = parser.parse_args()
 itraj = args.itraj
 
-cores = 5
-js = list(np.logspace(-3.7,-2.9,16))[:5]
+cores = 15
+js = list(np.logspace(-8,-5,160))
 
 def send_vans(ns):
-    for k in range(50,100):
-        os.system("{} running/tfq/HEA/main_HEA.py --params '{}' --lr 0.001 --L_HEA 1 --noise_strength {} --noisy 1 --itraj {} --run_name HEA_fixed".format(get_python(),str([1.0, 1.0]), ns, itraj+k))
+    for k in range(10):
+        os.system("{} running/tfq/HEA/main_HEA.py --params '{}' --n_qubits 8 --lr 0.001 --L_HEA 1 --noise_strength {} --noisy 1 --itraj {} --run_name HEA_fixed".format(get_python(),str([1.0, 1.0]), ns, itraj+k))
 
 with mp.Pool(cores) as p:
     p.map(send_vans, js)
